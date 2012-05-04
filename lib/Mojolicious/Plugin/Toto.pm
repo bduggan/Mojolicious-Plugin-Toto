51707b6cae31ad250eba8634e045563943895eaf
Brian Duggan <brian.duggan@nasa.gov>
2012-05-03 21:50:15 -0400

    bootstrap

--- lib/Mojolicious/Plugin/Toto.pm
+++ lib/Mojolicious/Plugin/Toto.pm
@@ -73,21 +73,32 @@ http://www.beer.dotcloud.com
 package Mojolicious::Plugin::Toto;
 use Mojo::Base 'Mojolicious::Plugin';
 use Mojo::ByteStream qw/b/;
+use File::Basename 'dirname';
+use File::Spec::Functions 'catdir';
+use Mojolicious::Plugin::Toto::Model;
+use Cwd qw/abs_path/;
 
-use Toto ();
 use strict;
 use warnings;
 
 our $VERSION = 0.08;
 
+sub _render_static {
+    my $c = shift;
+    my $what = shift;
+    $c->render_static($what);
+}
+
 sub register {
     my ($self, $app, $conf) = @_;
 
     my @menu = @{ $conf->{menu} || [] };
+    my $prefix = $conf->{prefix} || '';
     my %menu = @menu;
 
-    $app->routes->get('/toto/bootstrap-min.css')->to("Toto");
-    $app->routes->get('/toto/bootstrap-min.js')->to("Toto");
+    my $base = catdir(abs_path(dirname(__FILE__)), qw/Toto Assets/);
+    push @{$app->renderer->paths}, catdir($base, 'templates');
+    push @{$app->static->paths},   catdir($base, 'public');
 
     for my $controller (keys %menu) {
 
@@ -97,12 +108,13 @@ sub register {
             $first ||= $action;
             $app->log->debug("Adding route for $controller/$action");
             $app->routes->get(
-                "/$controller/$action" => sub {
+                "$prefix/$controller/$action" => { layout => "toto" } => sub {
                     my $c = shift;
                     my @found = map { glob "$_/$controller/$action.*" } $c->app->renderer->paths;
                     return if @found;
-                    $c->stash->{template}       = "plural";
-                    $c->stash->{template_class} = 'Toto';
+                    $c->app->renderer->classes->[0] = 'Toto';
+                    $c->stash->{template}           = "plural";
+                    $c->stash->{toto_prefix}        = $prefix;
                   } => {
                     controller => $controller,
                     action     => $action,
@@ -112,8 +124,8 @@ sub register {
         }
         my $first_action = $first;
         $app->routes->get(
-            "/$controller" => sub {
-                shift->redirect_to("$controller/$first_action");
+            "$prefix/$controller" => sub {
+                shift->redirect_to("$prefix/$controller/$first_action");
               } => "$controller"
         );
         $first = undef;
@@ -121,13 +133,14 @@ sub register {
             # TODO skip existing routes
             $first ||= $action;
             $app->routes->get(
-                "/$controller/$action/(*key)" => sub {
+                "$prefix/$controller/$action/(*key)" => sub {
                     my $c = shift;
                     $c->stash(instance => $c->model_class->new(key => $c->stash('key')));
                     my @found = map { glob "$_/$controller/$action.*" } $c->app->renderer->paths;
                     return if @found;
-                    $c->stash->{template}       = "single";
-                    $c->stash->{template_class} = 'Toto';
+                    $c->stash->{template}           = "single";
+                    $c->stash->{toto_prefix}        = $prefix;
+                    $c->app->renderer->classes->[0] = 'Toto';
                   } => {
                       controller => $controller,
                       action     => $action,
@@ -137,7 +150,7 @@ sub register {
         }
         my $first_key = $first;
         $app->routes->get(
-            "/$controller/default/(*key)" => sub {
+            "$prefix/$controller/default/(*key)" => sub {
                 my $c = shift;
                 my $key = $c->stash("key");
                 $c->redirect_to("$controller/$first_key/$key");
@@ -147,11 +160,11 @@ sub register {
     }
     my @controllers = grep !ref($_), @menu;
     my $first_controller = $controllers[0];
-    $app->routes->get('/' => sub { shift->redirect_to($first_controller) } );
+    $app->routes->get("$prefix/" => sub { shift->redirect_to($first_controller) } );
 
-    for ($app, Toto::app()) {
+    for ($app) {
         $_->helper( toto_config => sub { $conf } );
-        $_->helper( model_class => sub { $conf->{model_class} || "Toto::Model" });
+        $_->helper( model_class => sub { $conf->{model_class} || "Mojolicious::Plugin::Toto::Model" });
         $_->helper( controllers => sub { @controllers } );
         $_->helper(
             actions => sub {
@@ -185,4 +198,51 @@ sub register {
 }
 
 1;
+
+__DATA__
+
+@@ layouts/toto.html.ep
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN">
+<html>
+<head>
+<title><%= title %></title>
+%= base_tag
+%= stylesheet "bootstrap/css/bootstrap.min.css";
+<style>
+pre.toto_code {
+    float:right;
+    right:10%;
+    padding:5px;
+    border:1px grey dashed;
+    font-family:monospace;
+    position:absolute;
+    }
+</style>
+</head>
+<body>
+<div class="container">
+<div class="row">
+<div class="span1">&nbsp;</div>
+<div class="span11">
+    <ul class="nav nav-tabs">
+% for my $c (controllers) {
+        <li <%== $c eq $controller ? q[ class="active"] : "" =%>>
+            <%= link_to "$toto_prefix/$c" => begin =%><%= $c =%><%= end =%>
+        </li>
+% }
+    </ul>
+</div>
+</div>
+    <div class="tabbable tabs-left">
+% if (stash 'key') {
+%= include 'top_tabs_single';
+% } else {
+%= include 'top_tabs_plural';
+% }
+         <div class="tab-content" style='width:auto;'>
+         <%= content =%>
+         </div>
+    </div>
+</div>
+</html>
 
